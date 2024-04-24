3f3b7648036f9eb490ee378518c718e58f003e4c
irrangga <irranggaa@gmail.com>
2024-04-24 09:54:08 +0700

    create db migration users

---
+++ db/migrations/20240424021618_seed_data_users.up.sql
@@ -0,0 +1,6 @@
+BEGIN;
+INSERT INTO users
+VALUES ('01', 'budi');
+INSERT INTO users
+VALUES ('02', 'Nano');
+COMMIT;
--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	}
 
 	// SETUP DATABASE
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=allow TimeZone=Asia/Shanghai",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Shanghai",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
